0ce61b909c41dd845c6ca76d5121ac97a5bb1012
Дмитрий К <krotkovd@gmail.com>
2024-11-18 12:47:05 +0300

    Debug page styling

--- test.php
+++ test.php
@@ -6,6 +6,12 @@ require_once("Curl.php");
 
 $curl = new Curl();
 
-$curl->setUrl('http://localhost')->setMethod("POST");
+$curl->setUrl('https://ipinfo.io/217.13.124.105/geo');
+
+$curl->printDebug();
+
+$curl = new Curl();
+
+$curl->setUrl('https://api.nationalize.io/?name=dmitrii');
 
 $curl->printDebug();
