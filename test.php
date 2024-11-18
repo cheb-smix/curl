43f1d224a4fcc0d920ab419ad0f48ca3f7c059d2
Дмитрий К <krotkovd@gmail.com>
2024-11-18 15:16:28 +0300

    Add README

--- test.php
+++ test.php
@@ -12,10 +12,4 @@ $curl = new Curl();
 $curl->setUrl('https://api.nationalize.io/?name=dmitrii');
 $curl->printDebug();
 
-(new Curl())->parseCli("curl --location 'https://smix-soft.ru/api/v2/game/set-step' \
---header 'Content-Type: application/x-www-form-urlencoded' \
---data-urlencode 'game_id=2' \
---data-urlencode 'from=1:1' \
---data-urlencode 'to=1:2' \
---data-urlencode 'effectivity=2' \
---data-urlencode 'mask=0101010010001010101001100110100101001010010101'")->printDebug();
+(new Curl())->parseCli("curl --location --request GET 'https://ipinfo.io/147.83." . rand(0, 254) . "." . rand(2, 254) . "/geo'")->printDebug();
